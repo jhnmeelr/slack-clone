0c63e1322af4eeaff7fabf79f9e000cebbfb9af9
jm <jhn.meelr@gmail.com>
2018-02-24 15:08:12 +0200

    Register Form in React Using Semantic UI and GraphQL

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,9 +1,20 @@
+import bcrypt from 'bcrypt';
+
 export default {
   Query: {
     getUser: (parent, args, context) => context.models.User.findOne({ where: { id: args.id } }),
     allUsers: (parent, args, context) => context.models.User.findAll(),
   },
   Mutation: {
-    createUser: (parent, args, context) => context.models.User.create(args),
+    register: async (parent, { password, ...otherArgs }, context) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 12);
+        await context.models.User.create({ ...otherArgs, password: hashedPassword });
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
   },
 };
