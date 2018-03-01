16c98a8ad7185084149b66ecc63a63c0b4ae23a5
jm <jhn.meelr@gmail.com>
2018-03-01 09:18:03 +0200

    Sending, Verifying, and Refreshing JWT Tokens in Node.js

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ import { makeExecutableSchema } from 'graphql-tools';
 import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
 
 import models from './models';
+import { refreshTokens } from './auth';
 
 const SECRET = 'jano2en21oi3ofmn3oi10-1221eadmwok';
 const SECRET2 = 'njk2k13awmfp142512312-cqwokt3234o';
@@ -20,17 +22,42 @@ const app = express();
 
 app.use(cors('*'));
 
+const addUser = async (req, res, next) => {
+  const token = req.headers['x-token'];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (err) {
+      const refreshToken = req.headers['x-refresh-token'];
+      const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+      if (newTokens.token && newTokens.refreshToken) {
+        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
+        res.set('x-token', newTokens.token);
+        res.set('x-refresh-token', newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
+
+app.use(addUser);
+
 const graphqlEndpoint = '/graphql';
 
 app.use(
   graphqlEndpoint,
   bodyParser.json(),
-  graphqlExpress({
+  graphqlExpress(req => ({
     schema,
     context: {
-      models, user: { id: 1 }, SECRET, SECRET2,
+      models,
+      user: req.user,
+      SECRET,
+      SECRET2,
     },
-  }),
+  })),
 );
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
--- resolvers/team.js
+++ resolvers/team.js
@@ -1,12 +1,18 @@
+import formatErrors from '../formatErrors';
+
 export default {
   Mutation: {
     createTeam: async (parent, args, context) => {
       try {
         await context.models.Team.create({ ...args, owner: context.user.id });
-        return true;
+        return {
+          ok: true,
+        };
       } catch (err) {
-        console.log(err);
-        return false;
+        return {
+          ok: false,
+          errors: formatErrors(err),
+        };
       }
     },
   },
